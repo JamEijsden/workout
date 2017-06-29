c87935879089eb0abcb32f3d056094f26e8ab7bb
Jimmie Van Eijsden <jimmie.van-eijsden@teliacompany.com>
2017-06-29 20:49:09 +0200

    initial commit #2

---
+++ angular-frontend/src/app/auth/auth0-variables.ts
@@ -0,0 +1,11 @@
+interface AuthConfig {
+  clientID: string;
+  domain: string;
+  callbackURL: string;
+}
+
+export const AUTH_CONFIG: AuthConfig = {
+  clientID: 'LvMUFjGd3a3UzkWpcDE__JM0rZSTsa83',
+  domain: 'jameijsden.eu.auth0.com',
+  callbackURL: 'http://localhost:4200/callback'
+};
